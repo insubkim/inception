b078743c012954fe4518b0ca2072853e4b20a017
Insub Kim <j.j.kant1948@gmail.com>
2024-01-03 08:33:25 +0000

    fix: change ~ to /home to all path

--- dockers/srcs/requirements/mariadb/tools/start_mariadb.sh
+++ dockers/srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -5,13 +5,13 @@ rc default
 rc-service mariadb start
 
 
-sed -i "s/ROOT_PASS/$ROOT_PASS/" /root/mysql_secure_installation.sql
-mysql -u root < /root/mysql_secure_installation.sql
+sed -i "s/ROOT_PASS/$ROOT_PASS/" /app/mysql_secure_installation.sql
+mysql -u root < /app/mysql_secure_installation.sql
 
-mysql -u root -p$ROOT_PASS < /root/create_database.sql
-sed -i "s/WORDPRESS_DB_USER/$WORDPRESS_DB_USER/" /root/create_wordpressdb_user.sql
-sed -i "s/WORDPRESS_DB_PASS/$WORDPRESS_DB_PASS/" /root/create_wordpressdb_user.sql
-mysql -u root -p$ROOT_PASS < /root/create_wordpressdb_user.sql
+mysql -u root -p$ROOT_PASS < /app/create_database.sql
+sed -i "s/WORDPRESS_DB_USER/$WORDPRESS_DB_USER/" /app/create_wordpressdb_user.sql
+sed -i "s/WORDPRESS_DB_PASS/$WORDPRESS_DB_PASS/" /app/create_wordpressdb_user.sql
+mysql -u root -p$ROOT_PASS < /app/create_wordpressdb_user.sql
 
 rc-service mariadb stop
 
--- dockers/srcs/requirements/wordpress/tools/start.sh
+++ dockers/srcs/requirements/wordpress/tools/start.sh
@@ -20,11 +20,6 @@ if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	mv wp-cli.phar /usr/local/bin/wp
 	wp core download 
 	wp core config --dbhost=mariadb --dbname=wordpress --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASS
-	#chmod 777 /usr
-	#chmod 777 /usr/share
-	#chmod 777 /usr/share/webapps
-	#chmod 777 /usr/share/webapps/wordpress
-	#chmod 777 /usr/share/webapps/wordpress/*
 	wp core install --url=$WP_URL --title=$WP_TITLE --admin_name=$WP_GOD --admin_password=$WP_GOD_PASS --admin_email=$WP_GOD_EMAIL
 	wp user create $WP_USR1 $WP_USR1_EMAIL --role=author --user_pass=$WP_USR1_PASS
 fi
